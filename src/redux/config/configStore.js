caf43657de984618aeff2c92c5af4bf18c56422b
volant97 <volant97@naver.com>
2023-11-28 10:51:05 +0900

    reset

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,8 +1,9 @@
 import { createStore } from "redux";
 import { combineReducers } from "redux";
-import todos from "../modules/todos";
 
-const rootReducer = combineReducers({ todos });
+const rootReducer = combineReducers({
+  //
+});
 
 const store = createStore(rootReducer);
 
--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,58 +1,26 @@
 // import uuid from "react-uuid";
 import shortid from "shortid";
 
-const ADD_TODO = "todos/ADD_TODO";
-export const addTodo = (payload) => {
-  return {
-    type: ADD_TODO,
-    payload,
-  };
-};
-
-const DELETE_TODO = "DELETE_TODO";
-export const deleteTodo = (payload) => {
-  return {
-    type: DELETE_TODO,
-    payload,
-  };
-};
-
-const SWITCH_TODO = "SWITCH_TODO ";
-export const switchTodo = (payload) => {
-  return {
-    type: SWITCH_TODO,
-    payload,
-  };
-};
-
 const initialState = [
   {
     id: shortid.generate(),
-    title: "제목1",
-    body: "내용1",
+    title: "",
+    body: "",
     isDone: false,
-  },
-  {
-    id: shortid.generate(),
-    title: "제목2",
-    body: "내용2",
-    isDone: true,
   },
 ];
 
 // 리듀서
 const todos = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_TODO:
-      return [action.payload, ...state];
+    case "ADD_TODO":
+      return; //TODO: 여기 작성
 
-    case DELETE_TODO:
-      const id = action.payload;
-      const newTodos = state.filter((todo) => todo.id !== id);
-      return newTodos;
+    case "DELETE_TODO":
+      return; //TODO: 여기 작성
 
-    case SWITCH_TODO:
-      return [action.payload, ...state];
+    case "SWITCH_TODO":
+      return; //TODO: 여기 작성
 
     default:
       return state;
